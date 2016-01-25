3b39c6aa02406c1163502ecc933dc22304312faa
Tristan Michelet <tristan@datadoghq.com>
2016-01-25 15:50:20 -0500

    [Integrations] Add BUILD_ITERATION param

--- post.sh
+++ post.sh
@@ -11,7 +11,7 @@ fi
 
 if [[ $DOCKER_IMAGE == *"rpm"* ]]
 then
-  docker run -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg -v /home/ubuntu/cache:/var/cache/omnibus --entrypoint $ENTRYPOINT $DOCKER_IMAGE:sdk $COMMAND -l /dd-agent-omnibus/build_integration.sh
+  docker run -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION  -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg -v /home/ubuntu/cache:/var/cache/omnibus --entrypoint $ENTRYPOINT $DOCKER_IMAGE:sdk $COMMAND -l /dd-agent-omnibus/build_integration.sh
 else
-  docker run -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -e CORE_INTEGRATIONS_BRANCH=$CORE_INTEGRATIONS_BRANCH -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg -v /home/ubuntu/cache:/var/cache/omnibus --entrypoint $ENTRYPOINT $DOCKER_IMAGE:sdk $COMMAND -l /dd-agent-omnibus/build_integration.sh
+  docker run -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -e CORE_INTEGRATIONS_BRANCH=$CORE_INTEGRATIONS_BRANCH -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg -v /home/ubuntu/cache:/var/cache/omnibus --entrypoint $ENTRYPOINT $DOCKER_IMAGE:sdk $COMMAND -l /dd-agent-omnibus/build_integration.sh
 fi
