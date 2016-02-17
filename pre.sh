bb6ab836f00503e90d9dc72a6ad354e1344db638
Tristan Michelet <tristan@datadoghq.com>
2016-02-17 14:29:34 -0500

    [Integrations] Add INTEGRATIONS_REPO parameter

--- pre.sh
+++ pre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p ~/pkg ~/cache ~/keys ~/cache/$DOCKER_IMAGE
+mkdir -p ~/pkg ~/keys
 if [[ $DOCKER_IMAGE == *"rpm"* ]]
 then
   echo -e $RPM_GPG_KEY > /home/ubuntu/keys/RPM-SIGNING-KEY.private
