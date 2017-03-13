854b0ee6dd216be7da2dfefa5a63160dd0faacc0
Greg Meyer <greg.meyer@datadoghq.com>
2017-03-13 10:46:03 -0400

    adds build all integrations flag

--- post.sh
+++ post.sh
@@ -2,7 +2,7 @@
 
 if [[ $DOCKER_IMAGE == *"rpm"* ]]
 then
-  docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION  -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
+  docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e BUILD_ALL_INTEGRATIONS=$BUILD_ALL_INTEGRATIONS -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION  -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
 else
-  docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
+  docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ALL_INTEGRATIONS=$BUILD_ALL_INTEGRATIONS -e BUILD_ITERATION=$BUILD_ITERATION -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
 fi
