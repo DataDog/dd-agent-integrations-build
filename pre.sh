4941b8fb02c6b8b950447c98a58f2c173033cb00
Greg Meyer <greg.meyer@datadoghq.com>
2017-05-04 16:53:24 -0400

    small fix to suse

--- pre.sh
+++ pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/pkg ~/keys
-if [[ $DOCKER_IMAGE == *"rpm"* ]]
+if [[ $DOCKER_IMAGE == *"rpm"* || $DOCKER_IMAGE == *"sles"* ]]
 then
   echo -e $RPM_GPG_KEY > /home/ubuntu/keys/RPM-SIGNING-KEY.private
 fi
