a9daefcf4e0368dd93187ab2d45b3b2eed0036bd
Greg Meyer <gregory.meyer@datadoghq.com>
2017-05-04 16:37:06 -0400

    adds suse (#4)

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Builds are run in docker containers that use the same images as the Agent builds
 
 * https://github.com/DataDog/docker-dd-agent-build-deb-x64
 * https://github.com/DataDog/docker-dd-agent-build-rpm-x64
+* https://github.com/DataDog/docker-dd-agent-build-rpm-suse-x64
 
 The main script used by the build is [`build_integration.sh`](https://github.com/DataDog/dd-agent-omnibus/blob/master/build_integration.sh)
 from [dd-agent-omnibus](https://github.com/DataDog/dd-agent-omnibus).
--- post.sh
+++ post.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [[ $DOCKER_IMAGE == *"rpm"* ]]
+if [[ $DOCKER_IMAGE == *"sles"* ]];
+then
+  docker login -e $QUAY_EMAIL -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+  docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION  -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
+elif [[ $DOCKER_IMAGE == *"rpm"* ]]
 then
   docker run -e INTEGRATIONS_REPO=$INTEGRATIONS_REPO -e INTEGRATION=$INTEGRATION -e VERSION=$VERSION -e BUILD_ITERATION=$BUILD_ITERATION  -e RPM_SIGNING_PASSPHRASE=$RPM_SIGNING_PASSPHRASE -e OMNIBUS_BRANCH=$OMNIBUS_BRANCH -e OMNIBUS_RUBY_BRANCH=$OMNIBUS_RUBY_BRANCH -v /home/ubuntu/keys:/keys -v /home/ubuntu/pkg:/dd-agent-omnibus/pkg --entrypoint bin/bash $DOCKER_IMAGE:latest -l /dd-agent-omnibus/build_integration.sh
 else
